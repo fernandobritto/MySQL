91732dc4ee0ae42a0ef82a8f606529da6f70707b
Fernando Britto <me@fernandobritto.com.br>
2018-03-28 15:47:27 -0300

    Initial Commit

--- Módulo 01/03_adicionando_registros.sql
+++ Módulo 01/03_adicionando_registros.sql
@@ -1,4 +1,5 @@
 -- Adicionando dados na Tabela
+-- Execute a instrução a seguir para inserir informações na tabela books
 
 INSERT INTO books ( title, author, price)
 	VALUES ("Aprendendo SQL", "Beaulieu", 54.90),
