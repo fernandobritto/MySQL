bc8fb39e6b3e371f083bf996ff3afe0c429a2e07
Fernando Britto <fernandobritto.web@gmail.com>
2018-05-20 10:32:56 -0300

    Função de string LPAD e RPAD

---
+++ Módulo 03/09_funcao_lpad_e_rpad.sql
@@ -0,0 +1,10 @@
+
+select lpad('Info Enter', 20, '* * * ');
+
+select lpad('coluna', 30, 'fica do lado esquerdo') from tabela;
+
+
+
+select rpad('Info Enter', 20, ' * * *');
+
+select rpad('coluna', 30, 'lado direito') from tabela;
