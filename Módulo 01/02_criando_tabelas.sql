43910e5facdacff1cc98a455053e1a1e59df3fbd
Fernando Britto <me@fernandobritto.com.br>
2018-11-07 09:15:31 -0200

    Criando uma Tabela

--- Módulo 01/02_criando_tabelas.sql
+++ Módulo 01/02_criando_tabelas.sql
@@ -1,6 +1,6 @@
 -- Criando uma Tabela
 CREATE TABLE clientes(
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     nome VARCHAR(255) NOT NULL,
     sobrenome VARCHAR(255) NOT NULL,
     
