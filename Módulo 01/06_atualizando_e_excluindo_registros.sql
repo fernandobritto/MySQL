100804f15c6c814bac522b811585bd6f76c12ec9
Fernando Britto <me@fernandobritto.com.br>
2018-12-10 10:56:25 -0200

    Atualizando e excluindo registros

--- Módulo 01/06_atualizando_e_excluindo_registros.sql
+++ Módulo 01/06_atualizando_e_excluindo_registros.sql
@@ -5,7 +5,7 @@ UPDATE livros
         
 
 DELETE FROM livros WHERE title = 'Banco de dados para iniciantes';
-DELETE from livros WHERE id = 2;
+DELETE FROM livros WHERE id = 2;
 
 
 -- Remove todas as informações da tabela sem excluir a tabela 
