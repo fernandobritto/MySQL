dfdb280fb61a5f2818df92b232439d637ea8571a
Fernando Britto <me@fernandobritto.com.br>
2019-04-24 12:58:40 -0300

    Adicionando Registros

--- Módulo 01/03_adicionando_registros.sql
+++ Módulo 01/03_adicionando_registros.sql
@@ -9,6 +9,13 @@ INSERT INTO clientes (nome, sobrenome, idade, endereco, email)
 		('Lyndon', 'Johnson', 36, 'Florianopolis - Santa Catarina', 'lylyonb_john@outlook.com.br'),			
 		('Richard', 'Nixon', 37, 'Rio de Janeiro - Rio de Janeiro', 'ninixon171@outlook.com'),			
 		('Spiro', 'Agnew', 37, 'Rio de Janeiro - Rio de Janeiro', 'spiro_agnew@yahoo.com'),
+		('Grover', 'Cleveland', 22, 'Salvador - Bahia', 'grover.cleveland@gmail.com'),			
+		('Theodore', 'Roosevelt', 26, 'Santos - São Paulo', 'theo_roosevelt@yahoo.com'),			
+		('Franklin', 'Roosevelt', 32, 'Santos - São Paulo', 'frankroosevelt@gmail.com'),			
+		('John', 'Kennedy', 35, 'Curitiba - Parana', 'Johnf_kennedy@outlook.com'),		
+		('Ronald', 'Reagan', 40, 'Aracaju - Sergipe', 'ronald_reagan@gmail.com'),			
+		('Warren', 'Harding', 29, 'Palmas - Tocantins', 'warren_g_harding@gmail.com'),
+		('Calvin', 'Coolidge', 29, 'Palmas - Tocantins', 'calvin_cool@gmail.com'),
 		('Charles', 'Dawes', 30, 'Guarulhos - São Paulo', 'charlinho_g_dawes@gmail.com'); 
 
 
